a0ce576cae6fcb5495346109ce2e1d6172716b45
chenjiaxi <chenjiaxi_byr@163.com>
2019-05-01 13:50:34 +0800

    fix(lint): fix some lint error and rules

--- services/index.js
+++ services/index.js
@@ -1,67 +1,59 @@
+/* eslint-disable no-console */
 const request = require('superagent');
-const axios = require('axios');
 
 module.exports = {
 
-    getToken: async (cookie,sessionId)=>{
+  getToken: async (cookie, sessionId) => {
+    try {
+      const p = new Promise(((resolve, reject) => {
+        request.get(`http://139.159.242.107/api/user/authorize/${sessionId}`)
+          .set('Cookie', cookie)
+        // .timeout({
+        //     response: 20000,
+        //     deadline: 20000,
+        // })
+          .end((err, res) => {
+            if (err) {
+              console.log(err);
+              reject();
+            } else {
+              resolve(res);
+            }
+          });
+      }));
 
-        try{
-            var p = new Promise(function(resolve, reject){
-                //request.get('http://39.104.84.131/api/user/authorize/'+ sessionId)
-                request.get('http://139.159.242.107/api/user/authorize/'+ sessionId)
-                .set('Cookie', cookie)
-                // .timeout({
-                //     response: 20000,
-                //     deadline: 20000,
-                // })
-                .end((err, res) => {
-                    if(err){
-                        console.log(err);
-                        reject();
-                    }else{
-                        resolve(res);
-                    }
-                });
-            }) 
+      return p;
+    } catch (e) {
+      throw e;
+    }
+  },
 
-            return p;
-                
-        } catch(e){
-            throw e;
-        }
-    },
+  checkToken: async (token) => {
+    try {
+      const p = new Promise(((resolve, reject) => {
+        request.post('http://account:8400/api/v1/account/check_token')
+        // .set('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8')
+          .type('application/x-www-form-urlencoded')
+          .send({
+            token
+          })
+          .timeout({
+            response: 60000,
+            deadline: 60000,
+          })
+          .end((err, res) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(res);
+            }
+          });
+      }));
 
-    checkToken: async (token)=>{
 
-        try{
-            
-            var p = new Promise(function(resolve, reject){ 
-                request.post('http://account:8400/api/v1/account/check_token')
-                // .set('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8')
-                .type('application/x-www-form-urlencoded') 
-                .send({
-                    token:token
-                })
-                .timeout({
-                    response: 60000,
-                    deadline: 60000,
-                })
-                .end((err, res) => {
-                    if(err){
-                        reject(err);
-                    }else{
-                        resolve(res);
-                    }
-                });
-            
-            
-            });
-            
-
-            return p;
-            
-        } catch(e){
-            throw e;
-        }
-    },
-}
+      return p;
+    } catch (e) {
+      throw e;
+    }
+  },
+};
